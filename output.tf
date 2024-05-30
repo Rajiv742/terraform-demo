f308029084adb76d7e37f8230055931979d02a6e
Rajiv Sinha <rajivsinha742@gmail.com>
2024-05-30 21:44:27 +0530

    making it work

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-output "demo_vpc_id" {
-  value = "${aws_vpc.demo_vpc.id}"
-}
+# output "demo_vpc_id" {
+#   value = "${aws_vpc.demo_vpc.id}"
+# }
 
 output "demo_app_server_public_id" {
   value = "${aws_instance.app_server.public_dns}"
@@ -10,9 +10,9 @@ output "demo_app_server_2_public_id" {
   value = "${aws_instance.app_server_2.public_dns}"
 }
 
-#DNS of LoadBalancer
-# output "lb_dns_name" {
-#   description = "DNS of Load balancer"
-#   value       = aws_lb.demo_loadbalancer.dns_name
-# }
+//DNS of LoadBalancer
+output "lb_dns_name" {
+  description = "DNS of Load balancer"
+  value       = aws_lb.demo_loadbalancer.dns_name
+}
 
